30edf7a7c3aefc0c436e63c799b8a7b39f2419e6
Anderson Tiago <andersonbonamim@gmail.com>
2025-05-05 20:28:01 -0300

    feat: iniciando implementação da tela Vendas

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -39,7 +39,7 @@ function Resumo() {
           </span>
         </div>
       </div>
-      <div className='box'>Gráficos</div>
+      <div className='box mb'>Gráficos</div>
     </section>
   )
 }
